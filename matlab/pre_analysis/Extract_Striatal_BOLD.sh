a5670344fa62b4b3b7fe0e7f5077018e2a51c0b0
ckorponay <64859234+ckorponay@users.noreply.github.com>
2026-05-14 21:44:28 -0400

    Update Extract_Striatal_BOLD.sh

--- matlab/pre_analysis/Extract_Striatal_BOLD.sh
+++ matlab/pre_analysis/Extract_Striatal_BOLD.sh
@@ -6,8 +6,8 @@ set subjList = ()
 
 foreach subj ($subjList)
 
-3dmaskdump -noijk  -mask /path/to/Striatal_Mask.nii.gz /path/to/denoised/{$subj}_fMRI_4D.nii.gz > /path/to/{$subj}_fMRI_4D.1D
+3dmaskdump -noijk  -mask /path/to/Striatal_Mask.nii.gz /path/to/denoised/{$subj}_fMRI_4D.nii.gz > /path/to/{$subj}_Striatal_fMRI_4D.1D
 
-1dcat -csvout /path/to/{$subj}_fMRI_4D.1D > /path/to/{$subj}_fMRI_4D.csv
+1dcat -csvout /path/to/{$subj}_Striatal_fMRI_4D.1D > /path/to/{$subj}_Striatal_fMRI_4D.csv
 
 end
